883a7f2843a44d5149a7d44339ef00cd8548d9b4
M. Thaler <mats68@gmail.com>
2020-10-23 00:16:21 +0200

    menu ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
-import styles from '../styles/Home.module.css'
-import 'materialize-css/dist/css/materialize.min.css';
-import {useState} from 'react'
+import styles from "../styles/Home.module.css";
+import "materialize-css/dist/css/materialize.min.css";
+import { useState } from "react";
+import { FcMenu } from "react-icons/fc";
 
 export default function Home() {
-
-  const [showSide, setShowSide]  = useState(true)
+  const [showSide, setShowSide] = useState(true);
   const onClick = () => {
-    setShowSide(cur => !cur)
-  }
+    setShowSide((cur) => !cur);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <button className={styles.menubutton} onClick={onClick}>go up</button>
+        <button
+          type='button'
+          className='btn btn-flat btn-large transparent waves-effect'
+          style={{fontSize: '30px'}}
+          onClick={onClick}>
+          <FcMenu />
+        </button>
       </div>
-      <div className={styles.sidebar} hidden={!showSide}>Sidebar</div>
+      <div className={styles.sidebar} hidden={!showSide}>
+        Sidebar
+      </div>
       <div className={showSide ? styles.main : styles.mainfull}>Main</div>
     </div>
   );
