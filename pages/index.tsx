f8b4ae46531fa7f7e8e4aab6eaa3778d75a125fe
M. Thaler <mats68@gmail.com>
2020-10-22 22:17:08 +0200

    added responsive CSS Grid Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.header}>Header</div>
+      <div className={styles.sidebar}>Sidebar</div>
+      <div className={styles.main}>Main</div>
+      <div className={styles.box}>
+        <div className={styles.box}>Box-1</div>
+        <div className={styles.box}>Box-2</div>
+        <div className={styles.box}>Box-3</div>
+        <div className={styles.box}>Box-4</div>
+      </div>
+      <div className={styles.footer}>Footer</div>
     </div>
-  )
+  );
 }
