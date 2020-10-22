cbccc978660c39e4989274bdfa08a473a6aedcca
M. Thaler <mats68@gmail.com>
2020-10-22 22:23:01 +0200

    added mat

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,6 @@ export default function Home() {
       <div className={styles.header}>Header</div>
       <div className={styles.sidebar}>Sidebar</div>
       <div className={styles.main}>Main</div>
-      <div className={styles.box}>
-        <div className={styles.box}>Box-1</div>
-        <div className={styles.box}>Box-2</div>
-        <div className={styles.box}>Box-3</div>
-        <div className={styles.box}>Box-4</div>
-      </div>
-      <div className={styles.footer}>Footer</div>
     </div>
   );
 }
