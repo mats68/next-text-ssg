5ec42fd279d1e58b60fa55467d993fb7049b077d
M. Thaler <mats68@gmail.com>
2020-10-22 23:56:34 +0200

    menu correct

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,18 @@ import {useState} from 'react'
 
 export default function Home() {
 
-  const [side, setSide]  = useState(false)
+  const [showSide, setShowSide]  = useState(true)
   const onClick = () => {
-     setSide(cur => !cur)
+    setShowSide(cur => !cur)
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <button onClick={onClick}>go up</button>
+        <button className={styles.menubutton} onClick={onClick}>go up</button>
       </div>
-      <div className={side ? styles.sidebarfull : styles.sidebar}>Sidebar</div>
-      <div className={styles.main}>Main</div>
+      <div className={styles.sidebar} hidden={!showSide}>Sidebar</div>
+      <div className={showSide ? styles.main : styles.mainfull}>Main</div>
     </div>
   );
 }
