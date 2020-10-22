4e077bd242de987336319c0a1f99e93af0c4e6fd
M. Thaler <mats68@gmail.com>
2020-10-22 23:37:39 +0200

    div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import styles from '../styles/Home.module.css'
+import 'materialize-css/dist/css/materialize.min.css';
+import {useState} from 'react'
 
 export default function Home() {
+
+  const [side, setSide]  = useState(false)
+  const onClick = () => {
+     setSide(cur => !cur)
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>Header</div>
-      <div className={styles.sidebar}>Sidebar</div>
+      <div className={styles.header}>
+        <button onClick={onClick}>go up</button>
+      </div>
+      <div className={side ? styles.sidebarfull : styles.sidebar}>Sidebar</div>
       <div className={styles.main}>Main</div>
     </div>
   );
