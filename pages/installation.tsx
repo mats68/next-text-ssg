f33e284952f268c7158ea2648a3119491972e39a
M. Thaler <mats68@gmail.com>
2020-10-23 19:34:54 +0200

    div

--- pages/installation.tsx
+++ pages/installation.tsx
@@ -1,11 +1,4 @@
-import { useEffect } from "react";
 import Layout from "../components/Layout";
-import {
-  AppLinks,
-  traverseLinks,
-  ILink,
-  ILinks,
-} from "../components/config/links";
 
 export default function Home() {
 
